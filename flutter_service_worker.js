f6f789ede0d4fd8a441187378ab644a66734dda0
awaisCh07 <awais.tariq@whiteboxtech.net>
2024-04-24 19:23:57 +0500

    re-upload 24 April

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -80,9 +80,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "b523f2b7266d288c97e89dcd4d95a891
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "42d34d9d383f8fe3589d7c472f45cdb6",
-"/": "42d34d9d383f8fe3589d7c472f45cdb6",
-"main.dart.js": "e663fb0adca58c1355c8a70966c0dd04",
+"index.html": "7a9b08cc61fbaa413fbecddd3a56229b",
+"/": "7a9b08cc61fbaa413fbecddd3a56229b",
+"main.dart.js": "cd59aa0cebfb77255769de1ee8a919a2",
 "manifest.json": "1714279b962025c04f7947780cbb2218",
 "version.json": "c70ea8b19e7ce89f9f2ba6f93bcd203e"};
 // The application shell files that are downloaded before a service worker can
