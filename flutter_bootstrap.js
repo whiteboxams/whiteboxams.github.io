e26b238719b9b33736aed83abda0bea0c7833226
Awais Chaudhry <awais.tariq@whiteboxtech.net>
2025-01-23 11:46:13 +0500

    live AMS

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1071811590"
+    serviceWorkerVersion: "1909676159"
   }
 });
