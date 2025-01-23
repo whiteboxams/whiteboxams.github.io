111281556bf66800f877a032a3220d7485a869a4
Awais Chaudhry <awais.tariq@whiteboxtech.net>
2025-01-23 11:29:24 +0500

    upgrader removed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "583627767"
+    serviceWorkerVersion: "1071811590"
   }
 });
