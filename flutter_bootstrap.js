1b329275b61ebff51132be452cf97a263a48f1d6
Awais Chaudhry <awais.tariq@whiteboxtech.net>
2025-02-12 19:02:36 +0500

    att

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2826080183"
+    serviceWorkerVersion: "445360147"
   }
 });
