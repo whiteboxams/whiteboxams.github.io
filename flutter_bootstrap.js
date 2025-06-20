554ee36c746dda1cdfd5fb2ea29549b992613ed9
Awais Chaudhry <124262295+awaisCh07@users.noreply.github.com>
2025-06-21 01:38:22 +0500

    fixed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4110680434"
+    serviceWorkerVersion: "3650347508"
   }
 });
